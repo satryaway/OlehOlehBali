adb3581661bc6f9d2535243566e6b491ef5f0756
Satria Agung M <satryaway@gmail.com>
2015-12-14 11:38:03 +0700

    Send data to database

--- app/src/main/java/com/jixstreet/oleholehbali/PreviewActivity.java
+++ app/src/main/java/com/jixstreet/oleholehbali/PreviewActivity.java
@@ -1,19 +1,31 @@
 package com.jixstreet.oleholehbali;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.jixstreet.oleholehbali.adapters.ProductListAdapter;
 import com.jixstreet.oleholehbali.adapters.ShoppingCartAdapter;
 import com.jixstreet.oleholehbali.models.Product;
 import com.jixstreet.oleholehbali.models.Transaction;
+import com.jixstreet.oleholehbali.utils.APIAgent;
 import com.jixstreet.oleholehbali.utils.CommonConstants;
 import com.jixstreet.oleholehbali.utils.Seeder;
+import com.jixstreet.oleholehbali.utils.Utility;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by satryaway on 12/9/2015.
@@ -27,6 +39,7 @@ public class PreviewActivity extends BaseActivity {
     private TextView paymentMethodTV;
     private int totalPayout;
     private TextView payoutTotalTV;
+    private Button finishBtn;
 
     @Override
     public int setView() {
@@ -65,11 +78,101 @@ public class PreviewActivity extends BaseActivity {
         postalCodeTV = (TextView) findViewById(R.id.postal_code_tv);
         paymentMethodTV = (TextView) findViewById(R.id.payment_method_tv);
         payoutTotalTV = (TextView) findViewById(R.id.payout_total_tv);
+        finishBtn = (Button) findViewById(R.id.finish_btn);
     }
 
     @Override
     public void setCallBack() {
         super.setCallBack();
+
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyTransaction();
+            }
+        });
+    }
+
+    private void applyTransaction() {
+        String url = CommonConstants.SERVICE_POST_TRANSACTION;
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage(getResources().getString(R.string.please_wait));
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put(CommonConstants.NAME, transaction.getName());
+        requestParams.put(CommonConstants.EMAIL, transaction.getEmail());
+        requestParams.put(CommonConstants.PHONE, transaction.getPhone());
+        requestParams.put(CommonConstants.ADDRESS, transaction.getAddress());
+        requestParams.put(CommonConstants.KEC, transaction.getSub_district());
+        requestParams.put(CommonConstants.KAB, transaction.getDistrict());
+        requestParams.put(CommonConstants.PROV, transaction.getProvince());
+        requestParams.put(CommonConstants.POSTAL_CODE, transaction.getPostal_code());
+        requestParams.put(CommonConstants.TOTAL, totalPayout);
+
+        String[] product_ids = new String[products.size()];
+        int[] qtys = new int[products.size()];
+        for (int i = 0; i < products.size(); i++) {
+            product_ids[i] = products.get(i).getId();
+            qtys[i] = products.get(i).getQuantity();
+
+            requestParams.put(CommonConstants.PRODUCT_ID + "[" + i + "]", product_ids[i]);
+            requestParams.put(CommonConstants.QTY + "[" + i + "]", qtys[i]);
+        }
+
+        requestParams.put(CommonConstants.PAYMENT_METHOD, paymentMethod);
+
+        APIAgent.post(url, requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onStart() {
+                progressDialog.show();
+            }
+
+            @Override
+            public void onFinish() {
+                progressDialog.hide();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    int status = response.getInt(CommonConstants.STATUS);
+                    if (status == CommonConstants.RESULT_OK_CODE) {
+                        makeAlertDialog(response.getString(CommonConstants.MESSAGE));
+                    } else {
+                        Utility.makeSnackBar(PreviewActivity.this, response.getString(CommonConstants.MESSAGE));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Utility.makeSnackBar(PreviewActivity.this, getResources().getString(R.string.RTO));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Utility.makeSnackBar(PreviewActivity.this, getResources().getString(R.string.SERVER_DOWN_MESSAGE));
+            }
+        });
+
+    }
+
+    private void makeAlertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton(CommonConstants.OK, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(PreviewActivity.this, ChooseCategoryActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     private void putData() {
--- app/src/main/java/com/jixstreet/oleholehbali/utils/CommonConstants.java
+++ app/src/main/java/com/jixstreet/oleholehbali/utils/CommonConstants.java
@@ -4,10 +4,11 @@ package com.jixstreet.oleholehbali.utils;
  * Created by satryaway on 12/6/2015.
  */
 public class CommonConstants {
-    public static final String LOCAL_SERVER = "http://10.125.242.159/";
+    public static final String LOCAL_SERVER = "http://192.168.1.1/";
     public static final String BASE_URL = LOCAL_SERVER + "oleholehbali/";
     public static final String SERVICE_GET_PRODUCT_BY_CATEGORY = BASE_URL+ "product/c/";
     public static final String SERVICE_GET_PRODUCT_IMAGE = BASE_URL+ "assets/images/";
+    public static final String SERVICE_POST_TRANSACTION = BASE_URL+ "transaction";
     public static final String COD = "Cash on Delivery";
     public static final String VIA_TRANSFER = "Via Transfer";
     public static final String VIA_CC = "Via Credit Card";
@@ -32,4 +33,17 @@ public class CommonConstants {
     public static int PRODUCT_CODE = 27;
     public static String TRANSACTION = "transaction";
     public static String PAYMENT_METHOD = "payment_method";
+    public static String EMAIL = "email";
+    public static String PHONE = "phone";
+    public static String ADDRESS = "address";
+    public static String KEC = "kec";
+    public static String KAB = "kab";
+    public static String PROV = "prov";
+    public static String POSTAL_CODE = "postal_code";
+    public static String TOTAL = "total";
+    public static String PRODUCT_ARRAY = "product_id[]";
+    public static String QTY_ARRAY = "qty[]";
+    public static String OK = "OK";
+    public static String PRODUCT_ID = "product_id";
+    public static String QTY = "qty";
 }
